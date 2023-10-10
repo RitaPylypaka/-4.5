8763202cf178a641fa7e4afaf8b5a5a9dbfe24a9
RitaPylypaka <ritapylypaka@gmail.com>
2023-10-10 18:48:02 +0300

    Add project files.

---
+++ пр4.5/пр4.5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		if ((y >= (x - 2) * (x - 2) - 3 && y <= x && y >= 0) ||
+			(y >= (x - 2) * (x - 2) - 3 && y <= -x && x >= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+		for (int i = 0; i < 10; i++)
+		{
+			x = -1. + rand() % 7;
+			y = -3 + rand() % 9 ;
+			if ((y >= (x - 2) * (x - 2) - 3 && y <= x && y >= 0) ||
+				(y >= (x - 2) * (x - 2) - 3 && y <= -x && x >= 0))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+		return 0;
+}
